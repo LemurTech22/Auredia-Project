1f396c0b47e5883a929bc1eaf69f690b8d59f96e
LemurTech <joseconde101jc@gmail.com>
2025-04-13 12:55:37 -0500

    Revised variables names

--- App/database.py
+++ App/database.py
@@ -3,15 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(100),nullable=False,unique=True)
-    fname = db.Column(db.String(50), nullable=False)
-    lname = db.Column(db.String(50), nullable=False)
+
+    employee_id = db.Column(db.Integer,primary_key=True)
+    user_name = db.Column(db.String(100),nullable=False,unique=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100),nullable=False)
 
     def to_dict(self):
-        return {"id": self.id, 
-                "username": self.username,
-                "fname": self.fname,
-                "lname": self.lname,
+        return {"id": self.employee_id, 
+                "user_name": self.user_name,
+                "first_name": self.first_name,
+                "last_name": self.last_name,
                 "email": self.email}
--- App/routes.py
+++ App/routes.py
@@ -3,55 +3,48 @@ from App.database import db, User
 
 routes = Blueprint('routes', __name__)
 
-#define the database here
-
-#creates a user and inserts into database
+#API core functionality
 @routes.route('/create_user',methods =['POST'])
 def create_user(): 
     data = request.get_json()
     user = User(
-        username=data['username'],
-        fname=data['fname'],
-        lname=data['lname'],
+        user_name=data['user_name'],
+        first_name=data['first_name'],
+        last_name=data['last_name'],
         email=data['email']
         )
-
-    #insert into database changes here
     db.session.add(user)
     db.session.commit()
-    #return a new user in a json with a status of creating a new user.
     return  jsonify(user.to_dict()), 201
     
-#get a user information
 @routes.route('/get_user/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    user = User.query.get(user_id)#get database info here db.get(user_id)
+    user = User.query.get(user_id)
 
     if not user:
         return jsonify({"Message": "User does not exist"}), 404
     return jsonify(user.to_dict())
 
-#update the user information
 @routes.route('/update_user/<int:user_id>',methods=['PUT'])
 def update_user(user_id):
-    user = User.query.get(user_id)#database call to get user by id db.get(user_id)
+    user = User.query.get(user_id)
     if not user:
         return jsonify({"Error": "User not Found"}),404
     
     info = request.get_json()
-    user.username = info.get("username",user.username)
+    user.user_name = info.get("user_name",user.user_name)
+    user.first_name = info.get("first_name", user.first_name)
+    user.last_name = info.get("last_name", user.last_name)
     user.email = info.get('email',user.email)
 
     db.session.commit()
     return jsonify(user.to_dict())
-#delete user 
+
 @routes.route('/delete_user/<int:user_id>', methods =['DELETE'])
 def delete_user(user_id):
-    #get from data base the user 
     user_to_delete =  User.query.get(user_id)
-    #run sql command to delete user by user id
     if not user_to_delete:
         return (jsonify({"Message": "User not found"}))
     db.session.delete(user_to_delete)
     db.session.commit()
-    return jsonify({"Message": "User has been deleted."}), 404
+    return jsonify({"Message": "User has been deleted."}), 200
