d1d7884e9fbe19a98e05e6db14b3937d66af76c4
LemurTech <joseconde101jc@gmail.com>
2025-04-14 11:16:45 -0500

    updated version number to some compadibility issues and removed debug.

--- App/auth.py
+++ App/auth.py
@@ -5,8 +5,6 @@ def require_api_key(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         provided_key = request.headers.get('X-API-KEY')
-        current_app.logger.info("Provided API Key: %s", provided_key)
-        current_app.logger.info("Expected API Key: %s", current_app.config.get('API_KEY'))
         if not provided_key or provided_key != current_app.config.get('API_KEY'):
             return jsonify({'Message': "Invalid or missing API Key"}),401
         
